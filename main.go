a01046aeea86bea2b484daa004c3fd9e1922c417
Kvaara <karnavaara.jesse@gmail.com>
2024-09-24 18:13:44 +0300

    Replaced Previous Manager + Node Smoke Tests With Worker + Task Ones
    It's now possible to create Workers and Tasks for
    them

--- main.go
+++ main.go
@@ -2,125 +2,59 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 
 	"github.com/google/uuid"
-	"github.com/kvaara/go-orchestrator/manager"
-	"github.com/kvaara/go-orchestrator/node"
 	"github.com/kvaara/go-orchestrator/task"
 	"github.com/kvaara/go-orchestrator/worker"
 )
 
-// Creates a dummy container by utilizing our `Task` struct.
-func createContainer() (*task.Docker, *task.DockerResult) {
-	c := task.Config{
-		Name:  "test-container-1",
-		Image: "postgres:13",
-		Env: []string{
-			"POSTGRES_USER=cube",
-			"POSTGRES_PASSWORD=secret",
-		},
-	}
-
-	// Creates a new Docker API client on the host machine. The client is the ultimate arbiter of Docker commands.
-	dc, err := client.NewClientWithOpts(client.FromEnv)
-	d := task.Docker{
-		Client: dc,
-		Config: c,
-	}
-	if err != nil {
-		fmt.Printf("There was an error creating the Docker API client: %v\n", err)
-		return nil, nil
-	}
-
-	result := d.Run()
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil, nil
-	}
-
-	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
-	return &d, &result
-}
-
-func stopAndRemoveContainer(d *task.Docker, id string) *task.DockerResult {
-	result := d.StopAndRemove(id)
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil
-	}
-
-	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
-	return &result
-}
-
 func main() {
-	t := task.Task{
-		ID:     uuid.New(),
-		Name:   "Task-1",
-		State:  task.Pending,
-		Image:  "Image-1",
-		Memory: 1024,
-		Disk:   1,
-	}
-
-	te := task.TaskEvent{
-		ID:        uuid.New(),
-		State:     task.Pending,
-		Timestamp: time.Now(),
-		Task:      t,
-	}
-
-	fmt.Printf("task: %v\n", t)
-	fmt.Printf("task event: %v\n", te)
-
+	db := make(map[uuid.UUID]*task.Task)
 	w := worker.Worker{
-		Name:  "worker-1",
-		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]*task.Task),
-	}
-	fmt.Printf("worker: %v\n", w)
-	w.CollectStats()
-	w.RunTask()
-	w.StartTask()
-	w.StopTask()
-
-	m := manager.Manager{
-		Pending: *queue.New(),
-		TaskDb:  make(map[string][]*task.Task),
-		EventDb: make(map[string][]*task.TaskEvent),
-		Workers: []string{w.Name},
+		Name:      "Worker-1",
+		Queue:     *queue.New(),
+		Db:        db,
+		TaskCount: 0,
 	}
 
-	fmt.Printf("manager: %v\n", m)
-	m.SelectWorker()
-	m.UpdateTasks()
-	m.SendWork()
+	fmt.Printf("Queue should be empty so no Tasks should be run")
+	result := w.RunTaskInQueue()
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	time.Sleep(time.Second * 10)
 
-	n := node.Node{
-		Name:   "Node-1",
-		Ip:     "192.168.1.1",
-		Cores:  1,
-		Memory: 512,
-		Disk:   5,
-		Role:   "worker",
+	// Hmmm... The way we create Tasks isn't ideal.
+	// It's hard to tell what properties should be defined (i.e., what properties are required).
+	t := task.Task{
+		ID:    uuid.New(),
+		Name:  "Task-1",
+		State: task.Scheduled,
+		Image: "strm/helloworld-http",
 	}
 
-	fmt.Printf("node: %v\n", n)
-
-	fmt.Printf("create a test container\n")
-	dockerTask, createResult := createContainer()
-	if createResult.Error != nil {
-		fmt.Printf("%v", createResult.Error)
-		// Exits the main program with an exit code of 1, indicating an error.
-		os.Exit(1)
+	fmt.Println("Queue should have one Scheduled Task, which should be dequeued, run, and changed to Running.")
+	w.AddTaskToQueue(t)
+	result = w.RunTaskInQueue()
+	if result.Error != nil {
+		panic(result.Error)
 	}
-	time.Sleep(time.Second * 5)
+	t.ContainerID = result.ContainerId
 
-	fmt.Printf("stopping container %s\n", createResult.ContainerId)
-	_ = stopAndRemoveContainer(dockerTask, createResult.ContainerId)
+	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerID)
+	time.Sleep(time.Second * 30)
+
+	fmt.Printf("Queue should be empty so we add the same Task stated as Completed to stop it.")
+	t.State = task.Completed
+	w.AddTaskToQueue(t)
+	time.Sleep(time.Second * 30)
+
+	fmt.Printf("Queue should have one Completed Task, which should be dequeued, stopped, and changed to Completed: %s", t.ID)
+	result = w.RunTaskInQueue()
+	if result.Error != nil {
+		panic(result.Error)
+	}
 }
