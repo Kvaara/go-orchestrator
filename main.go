433afb893b406527a596c62b699cf1eee131ae6d
Kvaara <karnavaara.jesse@gmail.com>
2024-09-29 23:12:22 +0300

    Smoke Test for Newly Implemented API and Its Components

--- main.go
+++ main.go
@@ -2,59 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/golang-collections/collections/queue"
-
 	"github.com/google/uuid"
 	"github.com/kvaara/go-orchestrator/task"
 	"github.com/kvaara/go-orchestrator/worker"
 )
 
 func main() {
-	db := make(map[uuid.UUID]*task.Task)
+	host := os.Getenv("CUBE_HOST")
+	// Converts string to int:
+	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+
+	fmt.Println("Starting Cube worker")
+
 	w := worker.Worker{
-		Name:      "Worker-1",
-		Queue:     *queue.New(),
-		Db:        db,
-		TaskCount: 0,
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
 	}
+	api := worker.Api{Address: host, Port: port, Worker: &w}
 
-	fmt.Printf("Queue should be empty so no Tasks should be run")
-	result := w.RunTaskInQueue()
-	if result.Error != nil {
-		panic(result.Error)
-	}
-	time.Sleep(time.Second * 10)
+	api.ServeAPI()
 
-	// Hmmm... The way we create Tasks isn't ideal.
-	// It's hard to tell what properties should be defined (i.e., what properties are required).
-	t := task.Task{
-		ID:    uuid.New(),
-		Name:  "Task-1",
-		State: task.Scheduled,
-		Image: "strm/helloworld-http",
-	}
+	// The `go` keyword is used to create Goroutines (i.e., threads) to handle concurrent operations.
+	// This means that the below loop is nonblocking and moves on to the next statement.
+	go runTasks(&w)
+}
 
-	fmt.Println("Queue should have one Scheduled Task, which should be dequeued, run, and changed to Running.")
-	w.AddTaskToQueue(t)
-	result = w.RunTaskInQueue()
-	if result.Error != nil {
-		panic(result.Error)
-	}
-	t.ContainerID = result.ContainerId
-
-	fmt.Printf("task %s is running in container %s\n", t.ID, t.ContainerID)
-	time.Sleep(time.Second * 30)
-
-	fmt.Printf("Queue should be empty so we add the same Task stated as Completed to stop it.")
-	t.State = task.Completed
-	w.AddTaskToQueue(t)
-	time.Sleep(time.Second * 30)
-
-	fmt.Printf("Queue should have one Completed Task, which should be dequeued, stopped, and changed to Completed: %s", t.ID)
-	result = w.RunTaskInQueue()
-	if result.Error != nil {
-		panic(result.Error)
+func runTasks(w *worker.Worker) {
+	for {
+		if w.Queue.Len() != 0 {
+			result := w.RunTaskInQueue()
+			if result.Error != nil {
+				log.Printf("Error running task: %v\n", result.Error)
+			}
+		} else {
+			log.Printf("No tasks to process currently.\n")
+		}
+		log.Println("Sleeping for 10 seconds.")
+		time.Sleep(10 * time.Second)
 	}
 }
